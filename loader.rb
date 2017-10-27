41ef7f4de9c6de0236e2112607903e42e28a30db
Tatsuya Koishi <pooza@seas.b-shock.local>
2017-10-27 21:37:10 +0900

    refs #1

--- loader.rb
+++ loader.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
-ROOT_DIR = File.expand_path('..', __FILE__)
+#
+path = File.expand_path(__FILE__)
+while File.symlink?(path)
+  path = File.expand_path(File.readlink(path))
+end
+ROOT_DIR = File.expand_path('..', path)
 $LOAD_PATH.push(File.join(ROOT_DIR, 'lib'))
 ENV['BUNDLE_GEMFILE'] ||= File.join(ROOT_DIR, 'Gemfile')
 
