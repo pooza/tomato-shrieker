6f7a12f160f6be3a7d1c82e06af54ebc94c50dd3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-05-15 05:04:11 +0900

    #63

---
+++ standalone.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+path = File.expand_path(__FILE__)
+path = File.expand_path(File.readlink(path)) while File.symlink?(path)
+ROOT_DIR = File.expand_path('..', path)
+$LOAD_PATH.push(File.join(ROOT_DIR, 'lib'))
+ENV['BUNDLE_GEMFILE'] ||= File.join(ROOT_DIR, 'Gemfile')
+ENV['SSL_CERT_FILE'] ||= File.join(ROOT_DIR, 'cert/cacert.pem')
+
+require 'bundler/setup'
+require 'tomato-toot/standalone'
+
+TomatoToot::Standalone.new.execute
