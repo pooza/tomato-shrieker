0bdbdbce370614c175764c9c64b3da0460653af4
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-10-24 18:01:58 +0900

    #104

--- lib/tomato-toot/mastodon.rb
+++ lib/tomato-toot/mastodon.rb
@@ -1,5 +1,6 @@
 require 'addressable/uri'
 require 'httparty'
+require 'rest-client'
 require 'json'
 require 'tomato-toot/package'
 
@@ -12,10 +13,7 @@ module TomatoToot
     def toot(text, options = {})
       values = options.clone
       values[:status] = text
-      url = Addressable::URI.parse(@params['url'])
-      url.path = '/api/v1/statuses'
-
-      return HTTParty.post(url, {
+      return HTTParty.post(create_uri('/api/v1/statuses'), {
         body: values.to_json,
         headers: {
           'Content-Type' => 'application/json',
@@ -25,6 +23,26 @@ module TomatoToot
         },
         ssl_ca_file: ENV['SSL_CERT_FILE'],
       })
+    end
+
+    def upload(path)
+      response = RestClient.post(
+        create_uri('/api/v1/media').to_s,
+        {file: File.new(path, 'rb')},
+        {
+          'User-Agent' => Package.user_agent,
+          'Authorization' => "Bearer #{@token}",
+        },
+      )
+      return JSON.parse(response.body)['id'].to_i
+    end
+
+    private
+
+    def create_uri(href)
+      uri = Addressable::URI.parse(@params['url'])
+      uri.path = href
+      return uri
     end
   end
 end
