26258049408eda1b0f57bb667e036e7835e9bfe6
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-02-21 17:43:33 +0900

    #138

--- test/feed.rb
+++ test/feed.rb
@@ -1,8 +1,24 @@
 module TomatoToot
   class FeedTest < Test::Unit::TestCase
-    def test_new
+    def test_all
       Feed.all do |feed|
         assert(feed.is_a?(Feed))
+      end
+    end
+
+    def test_fetch
+      Feed.all do |feed|
+        feed.fetch do |entry|
+          assert(entry.is_a?(FeedEntry))
+        end
+      end
+    end
+
+    def test_fetch_all
+      Feed.all do |feed|
+        feed.fetch_all do |entry|
+          assert(entry.is_a?(FeedEntry))
+        end
       end
     end
 
@@ -27,6 +43,18 @@ module TomatoToot
     def test_toot_tags
       Feed.all do |feed|
         assert(feed.toot_tags.is_a?(Array))
+      end
+    end
+
+    def test_visibility
+      Feed.all do |feed|
+        assert(feed.visibility.is_a?(String))
+      end
+    end
+
+    def test_timestamp
+      Feed.all do |feed|
+        assert(feed.timestamp.is_a?(Time))
       end
     end
   end
