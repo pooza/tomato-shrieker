d3bc89084581070435bb7f52b238f497a60a7376
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-03-12 18:21:54 +0900

    #145

--- bin/crawl.rb
+++ bin/crawl.rb
@@ -5,7 +5,6 @@ path = File.expand_path(File.readlink(path)) while File.symlink?(path)
 dir = File.expand_path('../..', path)
 $LOAD_PATH.unshift(File.join(dir, 'lib'))
 ENV['BUNDLE_GEMFILE'] ||= File.join(dir, 'Gemfile')
-ENV['SSL_CERT_FILE'] ||= File.join(dir, 'cert/cacert.pem')
 
 require 'bundler/setup'
 require 'tomato_toot'
