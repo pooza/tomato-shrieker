b93787fd529e86e17155346a7c7f8b87159b6d24
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-15 12:50:52 +0900

    #182

--- Rakefile
+++ Rakefile
@@ -1,5 +1,5 @@
 dir = File.expand_path(__dir__)
-$LOAD_PATH.unshift(File.join(dir, 'lib'))
+$LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] ||= File.join(dir, 'Gemfile')
 
 require 'bundler/setup'
@@ -10,6 +10,6 @@ require 'tomato_toot'
   task action => "tomato:#{action}"
 end
 
-Dir.glob(File.join(TomatoToot::Environment.dir, 'lib/task/*.rb')).each do |f|
+Dir.glob(File.join(TomatoToot::Environment.dir, 'app/task/*.rb')).each do |f|
   require f
 end
---
+++ app/lib/tomato_toot.rb
@@ -0,0 +1,35 @@
+require 'bootsnap'
+require 'active_support'
+require 'active_support/core_ext'
+require 'zeitwerk'
+require 'ginseng'
+
+module TomatoToot
+  def self.dir
+    return File.expand_path('../..', __dir__)
+  end
+
+  def self.bootsnap
+    Bootsnap.setup(
+      cache_dir: File.join(dir, 'tmp/cache'),
+      load_path_cache: true,
+      autoload_paths_cache: true,
+      compile_cache_iseq: true,
+      compile_cache_yaml: true,
+    )
+  end
+
+  def self.loader
+    config = YAML.load_file(File.join(dir, 'config/autoload.yaml'))
+    loader = Zeitwerk::Loader.new
+    loader.inflector.inflect(config['inflections'])
+    loader.push_dir(File.join(dir, 'app/lib'))
+    config['dirs'].each do |d|
+      loader.push_dir(File.join(dir, 'app', d))
+    end
+    loader.setup
+  end
+end
+
+TomatoToot.bootsnap
+TomatoToot.loader
