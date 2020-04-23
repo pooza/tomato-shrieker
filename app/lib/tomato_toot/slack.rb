d219a156d464caabd153a0113a2100efc5ef9cdb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-04-23 14:13:05 +0900

    ginseng-core 1.6.6

--- app/lib/tomato_toot/slack.rb
+++ app/lib/tomato_toot/slack.rb
@@ -2,12 +2,6 @@ module TomatoToot
   class Slack < Ginseng::Slack
     include Package
 
-    def say(message, type = :yaml)
-      r = super(message, type)
-      raise Ginseng::GatewayError, "response #{r.code} (#{uri})" unless r.code == 200
-      return r
-    end
-
     def self.all
       return enum_for(__method__) unless block_given?
       Config.instance['/slack/hooks'].each do |url|
