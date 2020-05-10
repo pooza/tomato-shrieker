7cd59c2370ca269fecb8b2904440b971ad0eea3c
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-05-10 14:05:50 +0900

    #252

--- test/entry.rb
+++ test/entry.rb
@@ -1,41 +1,24 @@
 module TomatoToot
   class EntryTest < Test::Unit::TestCase
+    def setup
+      @entry = Entry.dataset.all.last
+    end
+
     def test_feed
-      Source.all do |source|
-        next unless source.is_a?(FeedSource)
-        source.fetch do |entry|
-          assert_kind_of(Feed, entry.feed)
-        end
-      end
+      assert_kind_of(FeedSource, @entry.feed)
     end
 
     def test_body
-      Source.all do |source|
-        next unless source.is_a?(FeedSource)
-        source.fetch do |entry|
-          assert(entry.body.present?)
-          assert_kind_of(String, entry.body)
-        end
-      end
+      assert(@entry.body.present?)
+      assert_kind_of(String, @entry.body)
     end
 
     def test_uri
-      Source.all do |source|
-        next unless source.is_a?(FeedSource)
-        source.fetch do |entry|
-          assert_kind_of(Ginseng::URI, entry.uri)
-        end
-      end
+      assert_kind_of(Ginseng::URI, @entry.uri)
     end
 
     def test_enclosure
-      Source.all do |source|
-        next unless source.is_a?(FeedSource)
-        source.fetch do |entry|
-          next if entry.enclosure.nil?
-          assert_kind_of(Ginseng::URI, entry.enclosure)
-        end
-      end
+      assert_kind_of(Ginseng::URI, @entry.enclosure) if @entry.enclosure
     end
   end
 end
