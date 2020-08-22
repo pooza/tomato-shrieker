9298c184ee4c4c9cc4a966ee19b161af35023a7e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-22 15:16:34 +0900

    #360

--- app/lib/tomato_shrieker/shrieker/misskey_shrieker.rb
+++ app/lib/tomato_shrieker/shrieker/misskey_shrieker.rb
@@ -10,6 +10,7 @@ module TomatoShrieker
         end
         body.delete(:attachments)
       end
+      body[:visibility] = Ginseng::Fediverse::NoteParser.visibility_name(body[:visibility])
       return note(body)
     end
   end
