895d31f946768adc50aa856eed89fc701d354463
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-10-20 09:06:41 +0900

    #383

--- app/task/purge.rb
+++ app/task/purge.rb
@@ -1,19 +1,21 @@
 namespace :tomato do
-  desc 'delete old entries'
-  task :purge do
-    TomatoShrieker::Source.all do |source|
-      next unless source.is_a?(TomatoShrieker::FeedSource)
-      date = source.purge
-      puts "#{source.id}: purge (older than #{date})" if date
+  namespace :purge do
+    desc 'delete old entries'
+    task :run do
+      TomatoShrieker::Source.all do |source|
+        next unless source.is_a?(TomatoShrieker::FeedSource)
+        date = source.purge
+        puts "#{source.id}: purge (older than #{date})" if date
+      end
     end
-  end
 
-  desc 'delete old entries (dryrun)'
-  task :purge_dryrun do
-    TomatoShrieker::Source.all do |source|
-      next unless source.is_a?(TomatoShrieker::FeedSource)
-      date = source.purge(dryrun: true)
-      puts "#{source.id}: purge dryrun (older than #{date})" if date
+    desc 'delete old entries (dryrun)'
+    task :dryrun do
+      TomatoShrieker::Source.all do |source|
+        next unless source.is_a?(TomatoShrieker::FeedSource)
+        date = source.purge(dryrun: true)
+        puts "#{source.id}: purge dryrun (older than #{date})" if date
+      end
     end
   end
 end
