5134593e1fa68c083d68e80e5fef8993813ca221
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-13 01:36:35 +0900

    #432

--- app/lib/tomato_shrieker/service/mulukhiya_service.rb
+++ app/lib/tomato_shrieker/service/mulukhiya_service.rb
@@ -1,18 +1,5 @@
-require 'sanitize'
-
 module TomatoShrieker
   class MulukhiyaService < Ginseng::Fediverse::MulukhiyaService
     include Package
-
-    def search_hashtags(text)
-      params = {
-        body: {q: Sanitize.clean(text)}.to_json,
-      }
-      tags = []
-      @http.post('/mulukhiya/api/tagging/tag/search', params).each_value do |entry|
-        tags.concat(entry['words'])
-      end
-      return tags.uniq.compact
-    end
   end
 end
