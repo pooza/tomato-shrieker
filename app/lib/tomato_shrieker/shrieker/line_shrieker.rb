96f4ecf6512ad68d442a246d54083aeccbded4f9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-02-18 06:01:17 +0900

    #481

--- app/lib/tomato_shrieker/shrieker/line_shrieker.rb
+++ app/lib/tomato_shrieker/shrieker/line_shrieker.rb
@@ -9,14 +9,13 @@ module TomatoShrieker
     end
 
     def exec(body)
+      body = body.clone
+      body[:template][:tag] = false
       return @http.post('/v2/bot/message/push', {
         headers: {'Authorization' => "Bearer #{@token}"},
         body: {
           to: @user_id,
-          messages: [{
-            type: 'text',
-            text: body[:template].to_s.strip,
-          }],
+          messages: [{type: 'text', text: body[:template].to_s.strip}],
         }.to_json,
       })
     end
