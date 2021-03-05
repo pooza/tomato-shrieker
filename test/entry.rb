313d71590aadbc2791a048e12e1cdfa5394b2ea2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-03-06 06:29:14 +0900

    #515

--- test/entry.rb
+++ test/entry.rb
@@ -9,20 +9,28 @@ module TomatoShrieker
     end
 
     def test_template
-      assert_kind_of(Template, @entries.find(&:template).template)
+      if entry = @entries.find(&:template)
+        assert_kind_of(Template, entry.template)
+      end
     end
 
     def test_uri
-      assert_kind_of(Ginseng::URI, @entries.find(&:uri).uri)
+      if entry = @entries.find(&:uri)
+        assert_kind_of(Ginseng::URI, entry.uri)
+      end
     end
 
     def test_enclosure
-      assert_kind_of(Ginseng::URI, @entries.find(&:enclosure).enclosure)
+      if entry = @entries.find(&:enclosure)
+        assert_kind_of(Ginseng::URI, entry.enclosure)
+      end
     end
 
     def test_tags
-      @entries.find {|v| v.tags.count.positive?}.tags.each do |tag|
-        assert_kind_of(String, tag)
+      if entry = @entries.find {|v| v.tags.count.positive?}
+        entry.tags.each do |tag|
+          assert_kind_of(String, tag)
+        end
       end
     end
   end
