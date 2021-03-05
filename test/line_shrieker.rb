c7be0cc4fc7fa822cf8b7ffbfa6d7334c9ce7418
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-03-05 19:29:40 +0900

    #489

--- test/line_shrieker.rb
+++ test/line_shrieker.rb
@@ -2,15 +2,13 @@ module TomatoShrieker
   class LineShriekerTest < TestCase
     def setup
       @config = Config.instance
-      channel = @config['/line/channels'].first
-      @shrieker = LineShrieker.new(channel['user_id'], channel['token'])
-
       @template = Template.new('common')
       @template[:status] = Time.now.to_s
+      @template[:source] = Source.all.find(&:line?)
     end
 
     def test_exec
-      assert_equal(@shrieker.exec(template: @template).code, 200)
+      assert_equal(@template[:source].line.exec(template: @template).code, 200)
     end
   end
 end
