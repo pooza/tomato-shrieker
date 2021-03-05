d55eabdd3dccc6f2f7c55a176ff84a2982dd9331
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-03-06 06:14:29 +0900

    #515

--- test/entry.rb
+++ test/entry.rb
@@ -1,27 +1,27 @@
 module TomatoShrieker
   class EntryTest < TestCase
     def setup
-      @entry = Entry.dataset.all.last
+      @entries = Entry.dataset.all.select(&:feed)
     end
 
     def test_feed
-      assert_kind_of(FeedSource, @entry.feed) if @entry
+      assert_kind_of(FeedSource, @entries.sample.feed)
     end
 
     def test_template
-      assert_kind_of(Template, @entry.template) if @entry
+      assert_kind_of(Template, @entries.find(&:template).template)
     end
 
     def test_uri
-      assert_kind_of(Ginseng::URI, @entry.uri) if @entry
+      assert_kind_of(Ginseng::URI, @entries.find(&:uri).uri)
     end
 
     def test_enclosure
-      assert_kind_of(Ginseng::URI, @entry.enclosure) if @entry&.enclosure
+      assert_kind_of(Ginseng::URI, @entries.find(&:enclosure).enclosure)
     end
 
     def test_tags
-      (@entry&.tags || []).each do |tag|
+      @entries.find {|v| v.tags.count.positive?}.tags.each do |tag|
         assert_kind_of(String, tag)
       end
     end
