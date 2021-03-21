a8a84b501c490f00bdc27f362b4d48d20594053a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-03-21 13:39:00 +0900

    #521

--- app/lib/tomato_shrieker/shrieker/line_shrieker.rb
+++ app/lib/tomato_shrieker/shrieker/line_shrieker.rb
@@ -1,23 +1,17 @@
 module TomatoShrieker
-  class LineShrieker
-    def initialize(user_id, token)
-      @http = HTTP.new
-      @http.base_uri = 'https://api.line.me'
-      @logger = Logger.new
-      @user_id = user_id
-      @token = token
+  class LineShrieker < Ginseng::LineService
+    include Package
+
+    def initialize(params = {})
+      super rescue nil
+      @id = params[:id]
+      @token = params[:token]
     end
 
     def exec(body)
       body = body.clone
       body[:template][:tag] = false
-      return @http.post('/v2/bot/message/push', {
-        headers: {'Authorization' => "Bearer #{@token}"},
-        body: {
-          to: @user_id,
-          messages: [{type: 'text', text: body[:template].to_s.strip}],
-        }.to_json,
-      })
+      return say(body[:template].to_s.strip)
     end
   end
 end
