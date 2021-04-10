bfb41d121bcda8d3bf71d35c16c4fd5e74450022
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-04-11 03:54:07 +0900

    #535

--- test/feed_source.rb
+++ test/feed_source.rb
@@ -48,13 +48,6 @@ module TomatoShrieker
       end
     end
 
-    def test_entries
-      FeedSource.all.select(&:keyword).each do |source|
-        source.fetch do |entry|
-        end
-      end
-    end
-
     def test_multi_entries_template
       FeedSource.all do |source|
         assert_kind_of([Template, NilClass], source.multi_entries_template)
