3d4306e2b5e42319bfe3056fc39729ce764fcdbf
pooza <tkoishi@b-shock.co.jp>
2021-04-12 08:30:52 +0900

    #530

--- test/lemmy_shrieker.rb
+++ test/lemmy_shrieker.rb
@@ -1,25 +1,22 @@
 module TomatoShrieker
   class LemmyShriekerTest < TestCase
     def setup
-      return unless @source = Source.all.find(&:lemmy?)
-      @shrieker = @source.shriekers.find {|v| v.is_a?(LemmyShrieker)}
+      source = Source.all.find(&:lemmy?)
+      @shrieker = source.lemmy
       @template = Template.new('common')
       @template[:status] = Time.now.to_s
-      @template[:source] = @source
+      @template[:source] = source
     end
 
     def test_client
-      return unless @shrieker
       assert_kind_of(Faye::WebSocket::Client, @shrieker.client)
     end
 
     def test_login
-      return unless @shrieker
       @shrieker.login
     end
 
     def test_exec
-      return unless @shrieker
       assert(@template[:source].lemmy.exec(template: @template))
     end
   end
