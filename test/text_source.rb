c019310783e4643ba9d6c1e2364e8e6fb5c9a977
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-09-24 21:05:37 +0900

    #674

--- test/text_source.rb
+++ test/text_source.rb
@@ -1,4 +1,10 @@
 module TomatoShrieker
   class TextSourceTest < TestCase
+    def test_text
+      TextSource.all do |source|
+        assert_kind_of(String, source.text)
+        assert(source.text.present?)
+      end
+    end
   end
 end
