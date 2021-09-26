89b613f59a9c74b49d01d3a8c6bef4b62a4a8b90
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-09-26 10:38:22 +0900

    #683

--- app/lib/tomato_shrieker/service/slack_service.rb
+++ app/lib/tomato_shrieker/service/slack_service.rb
@@ -1,11 +1,11 @@
 module TomatoShrieker
-  class Slack < Ginseng::Slack
+  class SlackService < Ginseng::Slack
     include Package
 
     def self.all
       return enum_for(__method__) unless block_given?
       config['/slack/hooks'].each do |url|
-        yield Slack.new(url)
+        yield SlackService.new(url)
       end
     end
   end
