e39cb82b774ac443accc7837316f0d99c8468def
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-12-12 14:22:52 +0900

    #731

--- app/lib/tomato_shrieker/template.rb
+++ app/lib/tomato_shrieker/template.rb
@@ -1,6 +1,15 @@
 module TomatoShrieker
   class Template < Ginseng::Template
     include Package
+    attr_reader :path
+
+    def initialize(name)
+      @path = name if name.start_with?('/') && File.exist?(name)
+      @path ||= File.join(environment_class.dir, dir, "#{name.sub(/\.erb$/, '')}.erb")
+      raise RenderError, "Template file #{name} not found" unless File.exist?(@path)
+      @erb = ERB.new(File.read(@path), **{trim_mode: '-'})
+      self.params = {}
+    end
 
     def source
       return params[:source] || params[:feed]
