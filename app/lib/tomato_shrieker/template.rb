758e8158b5dd13bae8a5856883df996c5d18c184
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-12-13 05:38:47 +0900

    #731

--- app/lib/tomato_shrieker/template.rb
+++ app/lib/tomato_shrieker/template.rb
@@ -1,13 +1,6 @@
 module TomatoShrieker
   class Template < Ginseng::Template
     include Package
-    attr_reader :path
-
-    def initialize(name)
-      @path = name if name.start_with?('/') && File.exist?(name)
-      @path ||= File.join(environment_class.dir, dir, "#{name.sub(/\.erb$/, '')}.erb")
-      super(@path)
-    end
 
     def source
       return params[:source] || params[:feed]
