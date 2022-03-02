30cf5339810592111d4bbb6ff4b746caf8078167
pooza <tkoishi@b-shock.co.jp>
2022-03-02 09:17:13 +0900

    #782

--- bin/test.rb
+++ bin/test.rb
@@ -4,9 +4,9 @@ $LOAD_PATH.unshift(File.join(File.expand_path('..', __dir__), 'app/lib'))
 require 'tomato_shrieker'
 module TomatoShrieker
   warn Package.full_name
-  warn 'テストローダー'
+  warn File.basename(__FILE__)
   warn ''
-  TestCase.load(ARGV.getopts('', 'cases:')['cases'])
+  TestCase.load(ARGV.getopts('', 'cases:')['cases'] || ARGV.first)
 rescue => e
   warn e.message
   exit 1
