22cf0cc02b1d380c9cda76bc92d6c5301cf54440
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-06-25 15:19:48 +0900

    #845

---
+++ app/lib/tomato_shrieker/source/git_hub_repository_source.rb
@@ -0,0 +1,34 @@
+module TomatoShrieker
+  class TweetTimelineSource < FeedSource
+    def uri
+      uri = Ginseng::URI.parse(config['/tweet/urls/root'])
+      uri.path = File.join('/', account, 'rss')
+      return uri
+    end
+
+    def account
+      return self['/source/tweet/account']
+    end
+
+    alias every period
+
+    def ignore_entry?(entry)
+      return true if entry.title&.match?(/^(RT by|R to)[[:blank:]]*/)
+      return super
+    end
+
+    def create_record(entry)
+      values = entry.to_h
+      uri = Ginseng::URI.parse(values['url'])
+      uri.host = 'twitter.com'
+      uri.fragment = nil
+      values['url'] = uri.to_s
+      return super(values)
+    end
+
+    def self.all(&block)
+      return enum_for(__method__) unless block
+      Source.all.select {|s| s.is_a?(TweetTimelineSource)}.each(&block)
+    end
+  end
+end
