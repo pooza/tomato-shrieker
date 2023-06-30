b66066232743a040ebe98e96b02c072ef9934635
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-06-30 14:39:48 +0900

    #1031

--- test/lemmy_shrieker.rb
+++ test/lemmy_shrieker.rb
@@ -5,24 +5,6 @@ module TomatoShrieker
       return super
     end
 
-    def test_client
-      Source.all.select(&:test?).select(&:lemmy).each do |source|
-        assert_kind_of(Faye::WebSocket::Client, source.lemmy.client)
-      end
-    end
-
-    def test_verify_peer?
-      Source.all.select(&:test?).select(&:lemmy).each do |source|
-        assert_boolean(source.lemmy.verify_peer?)
-      end
-    end
-
-    def test_root_cert_file
-      Source.all.select(&:test?).select(&:lemmy).each do |source|
-        assert_path_exist(source.lemmy.root_cert_file)
-      end
-    end
-
     def test_exec
       Source.all.select(&:test?).select(&:lemmy).each do |source|
         source.clear
