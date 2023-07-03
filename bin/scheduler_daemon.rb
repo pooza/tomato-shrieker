ca53c16f29e0686f2e9c41f25a4e0a952ff360a8
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-07-04 06:01:16 +0900

    #1037

--- bin/scheduler_daemon.rb
+++ bin/scheduler_daemon.rb
@@ -4,4 +4,8 @@ $LOAD_PATH.unshift(File.join(File.expand_path('..', __dir__), 'app/lib'))
 ENV['RAKE'] = nil
 
 require 'tomato_shrieker'
-TomatoShrieker::SchedulerDaemon.spawn!
+module TomatoShrieker
+  include Package
+  SchedulerDaemon.spawn!
+  puts config.secure_dump.to_yaml
+end
