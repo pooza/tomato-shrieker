465f1b349065a52dc9e9016a472ae08da65c486e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-02-10 15:35:23 +0900

    #1114

--- app/lib/tomato_shrieker/source/icalendar_source.rb
+++ app/lib/tomato_shrieker/source/icalendar_source.rb
@@ -25,10 +25,34 @@ module TomatoShrieker
 
     def entries(&block)
       return enum_for(__method__) unless block
-      feedjira.entries
-        .sort_by {|entry| entry.published.to_f}
-        .each {|entry| entry.title = entry.title&.escape_status}
+      ical.events
+        .sort_by {|entry| entry.dtstart.to_f}
+        .each {|entry| create_entry(entry)}
         .each(&block)
+    end
+
+    def ignore_entry?(entry)
+      return true if keyword && !hot_entry?(entry)
+      return true if negative_keyword && negative_entry?(entry)
+      return false
+    end
+
+    def hot_entry?(entry)
+      return entry.summary&.match?(keyword) || entry.description&.match?(keyword)
+    end
+
+    def negative_entry?(entry)
+      return true if entry.summary&.match?(negative_keyword)
+      return true if entry.description&.match?(negative_keyword)
+      return false
+    end
+
+    def create_entry(entry)
+      entry.start_date = entry.dtstart.getlocal
+      entry.end_date = entry.dtend.getlocal
+      entry.title = entry.summary&.escape_status
+      entry.body = entry.summary&.description
+      return entry
     end
 
     def templates
