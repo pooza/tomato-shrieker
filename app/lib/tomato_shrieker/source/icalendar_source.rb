40a7e238529f474299168fe990587bdf941db8c3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-05-11 11:26:42 +0900

    #1150

--- app/lib/tomato_shrieker/source/icalendar_source.rb
+++ app/lib/tomato_shrieker/source/icalendar_source.rb
@@ -108,7 +108,7 @@ module TomatoShrieker
       uri = Ginseng::URI.parse(self['/source/calendar'])
       uri ||= Ginseng::URI.parse(self['/source/ical'])
       return nil unless uri&.absolute?
-      uri.query_values = {t: rand(1000..9999)}
+      uri.query_values = {t: Time.now.to_f.to_s}
       return uri
     end
 
