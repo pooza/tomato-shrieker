b1399d44d531bb6b4ed937045141d67ed5d64754
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-08-05 09:56:24 +0900

    #1301

--- bin/scheduler_worker.rb
+++ bin/scheduler_worker.rb
@@ -4,4 +4,6 @@ $LOAD_PATH.unshift(File.join(File.expand_path('..', __dir__), 'app/lib'))
 ENV['RAKE'] = nil
 
 require 'tomato_shrieker'
+TomatoShrieker.setup_database
+TomatoShrieker.loader.eager_load
 TomatoShrieker::Scheduler.instance.exec
