2834ec9900e4dd0a5a71f5c149cedf870b3abc3e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-08-06 09:28:16 +0900

    #1302

--- app/lib/tomato_shrieker/daemon/scheduler_daemon.rb
+++ app/lib/tomato_shrieker/daemon/scheduler_daemon.rb
@@ -18,23 +18,23 @@ module TomatoShrieker
       super
     end
 
-def fork!(args)
-  Process.setsid
-  exit if fork
-  File.write(pid_file, Process.pid.to_s)
-  Dir.chdir(working_dir)
+    def fork!(args)
+      Process.setsid
+      exit if fork
+      File.write(pid_file, Process.pid.to_s)
+      Dir.chdir(working_dir)
 
-  $stdout.reopen('/dev/null', 'w')
-  $stderr.reopen('/dev/null', 'w')
-  $stdout.sync = true
-  $stderr.sync = true
+      $stdout.reopen('/dev/null', 'w')
+      $stderr.reopen('/dev/null', 'w')
+      $stdout.sync = true
+      $stderr.sync = true
 
-  trap('TERM') do
-    stop
-    exit
-  end
-  start(args)
-end
+      trap('TERM') do
+        stop
+        exit
+      end
+      start(args)
+    end
 
     def start(args)
       save_config
--- app/lib/tomato_shrieker/scheduler.rb
+++ app/lib/tomato_shrieker/scheduler.rb
@@ -8,13 +8,25 @@ module TomatoShrieker
       Source.all.reject(&:disable?).each do |source|
         source.load
         if source.post_at
-          job = @scheduler.at(source.post_at, {tag: source.id}) {source.exec}
-          logger.info(source: source.id, job:, class: source.class.to_s, at: source.post_at )
+          job = @scheduler.at(source.post_at, {tag: source.id}) do
+            logger.info(source: source.id, class: source.class.to_s, action: 'exec start', at: source.post_at)
+            source.exec
+            logger.info(source: source.id, class: source.class.to_s, action: 'exec end')
+          end
+          logger.info(source: source.id, job:, class: source.class.to_s, at: source.post_at)
         elsif source.cron
-          job = @scheduler.cron(source.cron, {tag: source.id}) {source.exec}
+          job = @scheduler.cron(source.cron, {tag: source.id}) do
+            logger.info(source: source.id, class: source.class.to_s, action: 'exec start', cron: source.cron)
+            source.exec
+            logger.info(source: source.id, class: source.class.to_s, action: 'exec end')
+          end
           logger.info(source: source.id, job:, class: source.class.to_s, cron: source.cron)
         else
-          job = @scheduler.every(source.period, {tag: source.id}) {source.exec}
+          job = @scheduler.every(source.period, {tag: source.id}) do
+            logger.info(source: source.id, class: source.class.to_s, action: 'exec start', every: source.every)
+            source.exec
+            logger.info(source: source.id, class: source.class.to_s, action: 'exec end')
+          end
           logger.info(source: source.id, job:, class: source.class.to_s, every: source.every)
         end
       end
