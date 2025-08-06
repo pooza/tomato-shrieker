7bc125dcb445f0da6017f644069da5ff28fec944
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-08-06 10:44:05 +0900

    #1305

--- app/lib/tomato_shrieker/scheduler.rb
+++ app/lib/tomato_shrieker/scheduler.rb
@@ -8,34 +8,13 @@ module TomatoShrieker
       Source.all.reject(&:disable?).each do |source|
         source.load
         if source.post_at
-          job = @scheduler.at(source.post_at, {tag: source.id}) do
-            logger.info(source: source.id, class: source.class.to_s, action: 'exec start',
-              at: source.post_at)
-            source.exec
-            logger.info(source: source.id, class: source.class.to_s, action: 'exec end')
-          rescue => e
-            logger.error(source: source.id, error: e)
-          end
+          job = register(source)
           logger.info(source: source.id, job:, class: source.class.to_s, at: source.post_at)
         elsif source.cron
-          job = @scheduler.cron(source.cron, {tag: source.id}) do
-            logger.info(source: source.id, class: source.class.to_s, action: 'exec start',
-              cron: source.cron)
-            source.exec
-            logger.info(source: source.id, class: source.class.to_s, action: 'exec end')
-          rescue => e
-            logger.error(source: source.id, error: e)
-          end
+          job = register(source)
           logger.info(source: source.id, job:, class: source.class.to_s, cron: source.cron)
         else
-          job = @scheduler.every(source.period, {tag: source.id}) do
-            logger.info(source: source.id, class: source.class.to_s, action: 'exec start',
-              every: source.every)
-            source.exec
-            logger.info(source: source.id, class: source.class.to_s, action: 'exec end')
-          rescue => e
-            logger.error(source: source.id, error: e)
-          end
+          job = register(source)
           logger.info(source: source.id, job:, class: source.class.to_s, every: source.every)
         end
       end
@@ -52,6 +31,17 @@ module TomatoShrieker
       @scheduler.cron('@hourly', 'purge') do
         FeedSource.purge_all
       end
+    end
+
+    def register(source)
+      job = @scheduler.at(source.post_at, {tag: source.id}) do
+        logger.info(source: source.id, class: source.class.to_s, action: 'exec start')
+        source.exec
+        logger.info(source: source.id, class: source.class.to_s, action: 'exec end')
+      rescue => e
+        logger.error(source: source.id, error: e)
+      end
+      return job
     end
   end
 end
