c7f3285ad176b34fa21f7065f2508e4da6420ab1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-08-06 19:30:27 +0900

    #1309

---
+++ bin/wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# tomato_shrieker_user, tomato_shrieker_path は rcスクリプトから export される前提
+
+: "${tomato_shrieker_user:=$(whoami)}"
+: "${tomato_shrieker_path:="/usr/home/${tomato_shrieker_user}/repos/tomato-shrieker"}"
+
+export HOME=$(eval echo "~${tomato_shrieker_user}")
+export PATH="${HOME}/.rbenv/bin:/usr/local/bin:${PATH}"
+export LANG=ja_JP.UTF-8
+export LC_ALL=ja_JP.UTF-8
+
+eval "$(rbenv init -)"
+cd "$tomato_shrieker_path" || exit 1
+exec bundle exec rake start
